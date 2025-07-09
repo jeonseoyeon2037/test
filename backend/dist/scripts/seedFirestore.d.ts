569827ecad9d3b18409e19efee4adee2331e2965
jeonseoyeon2037 <jseoyeon2037@gmail.com>
2025-07-10 06:00:26 +0900

    feature/sy: 전체 파일 삭제

--- backend/dist/scripts/seedFirestore.d.ts
+++
@@ -1,6 +0,0 @@
-import 'dotenv/config';
-export declare const seedPersonalSchedules: () => Promise<void>;
-export declare const seedDepartmentSchedules: () => Promise<void>;
-export declare const seedProjectSchedules: () => Promise<void>;
-export declare const seedAllData: () => Promise<void>;
-//# sourceMappingURL=seedFirestore.d.ts.map
